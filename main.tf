daac0e1fdae58dc635c96012fc3bea07981a8645
khadir108 <mohammadkhadir@outlook.com>
2020-03-25 14:22:41 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  credentials = file("terraform.json")
+  project = "hip-plexus-238517"
+  region  = "us-central1"
+  
+}
+
+resource "google_compute_instance" "vm-instance" {
+  name         = "target-vm"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+ 
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
